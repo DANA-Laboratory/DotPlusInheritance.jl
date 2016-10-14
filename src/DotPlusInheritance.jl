39b5fb01165b039f4c5c3441a2b63da0ecf10263
vimalaad <vimalaad@gmail.com>
2016-10-14 14:20:34 +0330

    v6

--- src/DotPlusInheritance.jl
+++ src/DotPlusInheritance.jl
@@ -1,22 +1,22 @@
-(.+)(var::Symbol,ex::Union(Expr,Symbol))=eval(DotPlusInheritance.redot(var,dotplus(typeof(eval(var)),ex)))
-dotplus(var::Symbol,ex::Union(Expr,Symbol))=DotPlusInheritance.redot(var,dotplus(typeof(eval(var)),ex))
-dotplus(var::DataType,ex::Union(Expr,Symbol))=DotPlusInheritance.dotplus(var,ex)
+(.+)(var::Symbol,ex::Union{Expr,Symbol})=eval(DotPlusInheritance.redot(var,dotplus(typeof(eval(var)),ex)))
+dotplus(var::Symbol,ex::Union{Expr,Symbol})=DotPlusInheritance.redot(var,dotplus(typeof(eval(var)),ex))
+dotplus(var::DataType,ex::Union{Expr,Symbol})=DotPlusInheritance.dotplus(var,ex)
 
-module DotPlusInheritance 
-  
-  inventory=Dict{DataType,Dict{Symbol,Union(Expr,Symbol)}}()
+module DotPlusInheritance
+
+  inventory=Dict{DataType,Dict{Symbol,Union{Expr,Symbol}}}()
   inventorytyp=Dict{DataType,Dict{Symbol,DataType}}()
 
-  function redot(from::Symbol,what::Union(Expr,Symbol))
+  function redot(from::Symbol,what::Union{Expr,Symbol})
     if isa(what,Symbol)
       return Expr(:.,from,QuoteNode(what))
     else
       return Expr(:.,redot(from,what.args[1]),what.args[2])
     end
   end
-  
+
   #return dot expression to address a variable of type with parent
-  function dotplus(t::DataType,ex::Union(Expr,Symbol))
+  function dotplus(t::DataType,ex::Union{Expr,Symbol})
     symbs=splitdotexpr(ex)
     ex=nothing
     while length(symbs)>0
@@ -34,7 +34,7 @@ module DotPlusInheritance
   end
 
   splitdotexpr(ex::Symbol)=[ex]
-  
+
   function splitdotexpr(ex::Expr,symbs=nothing)
     if symbs==nothing
       symbs=Array(Symbol,0)
@@ -56,45 +56,45 @@ module DotPlusInheritance
     end
     return inventory[t][sy],inventorytyp[t][sy]
   end
-  
+
   function addnamestoinventory(t::DataType)
-    item::Dict{Symbol,Union(Expr,Symbol)}=Dict{Symbol,Union(Expr,Symbol)}()
+    item::Dict{Symbol,Union{Expr,Symbol}}=Dict{Symbol,Union{Expr,Symbol}}()
     itemtyp::Dict{Symbol,DataType}=Dict{Symbol,DataType}()
     sym,exp,typ=allnames_recurcive(t)
-    for i in [1:length(sym)]
+    for i in 1:length(sym)
       item[sym[i]]=exp[i]
       itemtyp[sym[i]]=typ[i]
     end
     inventory[t]=item
     inventorytyp[t]=itemtyp
-    return 
+    return
   end
-  
+
   function allnames_recurcive(t::DataType)
     parentNames::Array{Symbol,1}=Array(Symbol,0)
     parentTyp::Array{DataType,1}=Array(DataType,0)
-    parentExprs::Array{Union(Expr,Symbol),1}=Array(Union(Expr,Symbol),0)
-    
+    parentExprs::Array{Union{Expr,Symbol},1}=Array(Union{Expr,Symbol},0)
+
     ret::Array{Symbol,1}=Array(Symbol,0)
-    retExpr::Array{Union(Expr,Symbol),1}=Array(Union(Expr,Symbol),0)
+    retExpr::Array{Union{Expr,Symbol},1}=Array(Union{Expr,Symbol},0)
     rettyp::Array{DataType,1}=Array(DataType,0)
-    
-    na=t.names
-    ty=t.types
+
+    na=fieldnames(t)
     j=1
-    for i in [1:length(na)]
-      if beginswith(string(na[i]),"_base_")
-        ap,axp,typ=allnames_recurcive(ty[i])
+    for i in 1:length(na)
+      ty=fieldtype(t,na[i])
+      if startswith(string(na[i]),"_base_")
+        ap,axp,typ=allnames_recurcive(ty)
         parentNames=vcat(parentNames,ap)
         parentTyp=vcat(parentTyp,typ)
-        for j in [1:length(axp)]
+        for j in 1:length(axp)
           axp[j]=redot(na[i],axp[j])
         end
         parentExprs=vcat(parentExprs,axp)
       else
         push!(ret,na[i])
         push!(retExpr,na[i])
-        push!(rettyp,ty[i])
+        push!(rettyp,ty)
       end
     end
     #over write parent variables
